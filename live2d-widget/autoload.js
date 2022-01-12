6736aa434add0f5bf98f1273a48d4663da834227
yanghaoi <21354684+yanghaoi@users.noreply.github.com>
2022-01-12 14:53:44 +0800

    Site updated: 2022-01-12 14:53:35

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,9 +1,7 @@
 // 注意：live2d_path 参数应使用绝对路径
 //const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-
-
-
-const live2d_path = "/live2d-widget/";
+//const live2d_path = "/live2d-widget/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/yanghaoi/yanghaoi.github.io/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
